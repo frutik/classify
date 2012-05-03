1c3308aefc51929f589a91e67daa953c3422dee5
Andrew Kornilov <frutik@gmail.com>
2012-05-03 15:39:18 +0300

    -

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='UA Classify',
+    version='1.0',
+    description='OpenShift App',
+    author='Frutik',
+    author_email='frutik@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Django=1.3.1'],
+)
